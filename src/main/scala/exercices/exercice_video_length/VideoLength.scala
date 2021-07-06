91892c3764e5518a7393d84e6a2ddca21a55e66c
aimebertrand <aime.bertrand@epitech.eu>
2021-07-06 10:17:36 +0200

    [ADDING] more error case

--- src/main/scala/exercices/exercice_video_length/VideoLength.scala
+++ src/main/scala/exercices/exercice_video_length/VideoLength.scala
@@ -18,13 +18,18 @@ object VideoLength {
     assert(longueur_to_seconds("01:00") == 60)
     assert(longueur_to_seconds("5:33") == 333)
     assert(longueur_to_seconds("61:00") == -1)
+    assert(longueur_to_seconds("10") == -1)
+    assert(longueur_to_seconds("10:7") == -1)
+    assert(longueur_to_seconds("5rig:30") == -1)
 
   }
 
   def isAllDigits(x: String) = x forall Character.isDigit
 
   def longueur_to_seconds(longueur: String): Int = {
+    if (!longueur.contains(':')) {return -1}
     var res = longueur.split(":")
+    if (res(1).length < 2) {return -1}
     if (!isAllDigits(res(0)) || !isAllDigits(res(1)))
       return (-1)
     val min = res(0).toInt
