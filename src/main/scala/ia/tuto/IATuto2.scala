4a66fd25465f432e737d9cf9131a870a7445a750
aimebertrand <aime.bertrand@epitech.eu>
2021-07-08 10:01:50 +0200

    [ADDING] exercice 2

--- src/main/scala/ia/tuto/IATuto2.scala
+++ src/main/scala/ia/tuto/IATuto2.scala
@@ -1,4 +1,4 @@
-/*package ia.tuto
+package ia.tuto
 
 
 import smile._
@@ -8,9 +8,15 @@ import scala.util.control.Breaks.{break, breakable}
 
 
 /*
+
+
 ### Exercice 1
+
 Essayer de comppléter les ??? pour pouvoir exécuter le programme.
+
+
 Voici un exemple de résultat attendu (les premières lignes peuvent changer comme les données sont triés aléatoirement):
+
 Data: DataLine(4.8,3.0,1.4,0.1,Iris-setosa); confidence: 0.98; Annotated: 1; Predicted: 1
 Data: DataLine(6.2,2.9,4.3,1.3,Iris-versicolor); confidence: 0.010638297872340425; Annotated: 0; Predicted: 0
 Data: DataLine(6.7,3.3,5.7,2.1,Iris-virginica); confidence: 0.010638297872340425; Annotated: 0; Predicted: 0
@@ -21,10 +27,19 @@ Data: DataLine(6.1,3.0,4.6,1.4,Iris-versicolor); confidence: 0.01063829787234042
 Data: DataLine(5.2,2.7,3.9,1.4,Iris-versicolor); confidence: 0.010638297872340425; Annotated: 0; Predicted: 0
 Data: DataLine(4.4,3.2,1.3,0.2,Iris-setosa); confidence: 0.98; Annotated: 1; Predicted: 1
 Data: DataLine(7.7,3.0,6.1,2.3,Iris-virginica); confidence: 0.010638297872340425; Annotated: 0; Predicted: 0
+
+
 Precision: 1.0;   Rappel: 1.0
+
+
+
+
 ### Exercice 2
+
 Essayer de faire la même chose avec le jeu de données suivant:
+
 https://github.com/bnjmn/weka/blob/master/wekadocs/data/segment-test.arff
+
  */
 
 
@@ -42,7 +57,7 @@ case class DataLine(
   // On choisit un type qui est associé à la valeur 1 et les autres à 0
   lazy val label : Int = {
     raw_data.split(",").last match {
-      case "Iris-setosa" => 1
+      case "window" => 1
       case _ => 0
     }
   }
@@ -52,10 +67,10 @@ case class DataLine(
   //  5.1,3.5,1.4,0.2
   lazy val features : Array[Double] = {
     raw_data.split(",").init.map(x => x.toDouble)
-    //test
+   //test
   }
 }
-object IATuto {
+object IATuto2 {
 
 
 
@@ -80,7 +95,7 @@ object IATuto {
     val test_data = {
       random_parsed_data
         .slice(
-          random_parsed_data.length - 10,
+          random_parsed_data.length - 15,
           random_parsed_data.length
         )
     }
@@ -176,10 +191,12 @@ object IATuto {
   }
 
   def splitIrisData() : Array[String] = {
-    val str = IrisData.txt_data
-    val tab = str.split('\n')
-    var i = 1
+
+    val bufferedSource = io.Source.fromFile("/home/aime_bertrand/Documents/my_first_scala_exercices/src/main/scala/ia/tuto/segment_data")
+    val tab = (for (line <- bufferedSource.getLines()) yield line).toArray
     val _map = new ArrayBuffer[String]()
+    var i = 1
+
 
     while (i < tab.length) {
       breakable {
@@ -192,5 +209,23 @@ object IATuto {
       i += 1
     }
     _map.toArray
+
+    /*val str = IrisData.txt_data
+    val tab = str.split('\n')
+    var i = 1
+
+
+    while (i < tab.length) {
+      breakable {
+        if (tab(i) == "")
+          break
+        else if (tab(i)(0) >= 48 && tab(i)(0) <= 57) {
+          _map+= tab(i)
+        }
+      }
+      i += 1
+    }
+    _map.toArray
+  */
   }
-}*/
+}
